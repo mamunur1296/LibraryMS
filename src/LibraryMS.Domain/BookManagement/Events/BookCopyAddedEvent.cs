2a2baf7c13509cdfaa147ad917a5df2c326be4f8
mamunbbit <programer597@gmail.com>
2026-07-29 18:01:19 +0600

    Refactor event records into separate files

---
+++ src/LibraryMS.Domain/BookManagement/Events/BookCopyAddedEvent.cs
@@ -0,0 +1,9 @@
+using System;
+using LibraryMS.Domain.Common;
+
+namespace LibraryMS.Domain.BookManagement.Events;
+
+public sealed record BookCopyAddedEvent(Guid BookId, Guid CopyId, Guid BranchId) : IDomainEvent
+{
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+}
